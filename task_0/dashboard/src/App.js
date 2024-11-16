208ab8d74a274402942d096e9a4dc0cf423faf4e
oluwafunmilola25 <ebunoluwaaribisala@gmail.com>
2024-11-16 15:36:40 -0500

    task 0

--- task_0/dashboard/src/App.js
+++ task_0/dashboard/src/App.js
@@ -8,11 +8,11 @@ function App() {
         <img src={holbertonLogo} alt="Holberton Logo" />
         <h1>School dashboard</h1>
         </header>
-        <body className="App-body">
+          <body className="App-body">
           <p>Login to access the full dashboard</p>
-        </body>
-        <footer className="App-footer">
-          <p> Copyright 2020 - holberton School </p>
+          </body>
+          <footer className="App-footer">
+          <p>Copyright 2020 - holberton School</p>
         </footer>
     </div>
     );
